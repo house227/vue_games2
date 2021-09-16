ce46dcbab7dd24bfa7463fd3a9bcfffb6376a803
house227 <house1000227@gmail.com>
2021-09-16 10:31:22 +0900

    詰み

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <div id="game">
+        <square 
+            :cnt="cnt"
+            v-for="n of 9"
+            :key="n"
+            @click="click_action"
+        />
+    </div>
+
+    <!-- @(v-on:)でクリックイベントを発生させる(カウンター) -->
+    <!-- v-bindは（：）に省略可 -->
+</template>
+
+
+<script>
+import square from './components/square.vue'
+export default{
+    name: 'App',
+    data(){
+        return{
+            // クリック回数cntを０で初期化
+            cnt:0,
+        };
+    },
+    components: {
+        square
+    },
+    methods:{
+        click_action(){
+            this.cnt++
+            
+        }
+    }
+};
+</script>
+
+<style >
+#game{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: 450px;
+}
+</style>
--- src/components/square.vue
+++ src/components/square.vue
@@ -1,31 +1,55 @@
 <template>
+<!-- マスに@(v-on:)でクリックイベント追加 -->
     <div class="cell" @click="action">
-       <p>{{ msg }}</p> 
+        <div> {{ square_cell }}</div>
     </div>
 
 </template>
 
 <script>
-
 export default{
-name:'square',
-props: {
-    msg: String
-},
-mounted(){
-    this.cnt = 0
-},
-methods:{
-    action(){
-        this.cnt++
-        if(cnt % 2 !== 0){
-            this.msg = '○'
-        }else{
-            this.msg = '✖'
-        }
-
-    }
-}
-
-}
+    name:'square',
+    data(){
+        return{
+            square_cell:" ",
+        };
+    },
+        // 親から送られてきたクリック回数をNumber型で受け取る
+    props: {
+        cnt: Number,
+        key: Number
+    },
+    mounted(){
+        // 子特有のカウンターをブラウザ起動時に０で初期化
+        this.bit_cnt = 0
+    },
+    methods:{
+        // クリックイベント
+        action(){
+            // 親から受け取ったクリック回数を元に条件分岐
+            // 子特有のカウンターbit_cntですでにクリックしたかを確認
+            // クリックされていればカウントが１なので条件が通らない。
+            if(this.cnt % 2 === 0 && this.bit_cnt === 0){
+                this.square_cell = "〇";
+                this.bit_cnt++
+            }else if(this.cnt % 2 !== 0 && this.bit_cnt === 0){
+                this.square_cell = "✖";
+                this.bit_cnt++
+            }
+        },
+    },
+};
 </script>
+
+<style>
+/* マスの枠デザイン */
+.cell{
+    border: 1px solid black;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+    line-height: 90px;
+    font-size: 30px;
+    padding-top: 50px;
+}
+</style>
